9e1ffe2b782a3335300ea20063cee5579e2a4d74
Mahesh Deore <maheshdeore791@gmail.com>
2024-04-27 23:19:13 +0530

    [DP-1] : Added static page and started httpd service

--- install_httpd.sh
+++ install_httpd.sh
@@ -2,3 +2,7 @@
 sudo yum update -y
 sudo yum install httpd -y
 sudo yum install git -y
+git clone https://github.com/maheshde791/app_deploy_ec2.git
+mv ./app_deploy_ec2/index.html /var/www/html
+sudo systemctl start httpd
+sudo systemctl enable httpd
