4196db76db9ec5dbfb23a2085a355dfcc248655f
Mahesh Deore <maheshdeore791@gmail.com>
2024-04-29 00:39:41 +0530

    Create backup.sh
    [DP-1] : Added backup script

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1. Create temporary folder
+temp_folder=$(mktemp -d)
+
+# 2. Copy apache logs from /var/log/httpd/access_log to temporary folder
+sudo cp /var/log/httpd/access_log "$temp_folder"
+
+# 3. Copy index.html from /var/www/html to temporary folder
+sudo cp /var/www/html/index.html "$temp_folder"
+
+# 4. Create backup zip with compression
+timestamp=$(date +%Y-%m-%d-%H%M%S)
+backup_file="$temp_folder/backup_$timestamp.zip"
+sudo zip -r "$backup_file" "$temp_folder"
+
+# 5. Move backup zip to backup folder
+backup_folder="$HOME/backup"
+sudo mv "$backup_file" "$backup_folder"
+
+echo "Backup created and moved to: $backup_folder"
